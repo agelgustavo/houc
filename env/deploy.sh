aa1b41402f00d5194749b6c185bb9919dae8470d
Gustavo Silverio <agelgustavo@gmail.com>
2015-06-19 03:09:27 -0300

    rename

--- env/deploy.sh
+++ env/deploy.sh
@@ -4,4 +4,4 @@ if [ ! -d "../lib" ]; then
     ln -s ~/.virtualenvs/houc/lib/python2.7/site-packages/ lib
 fi
 
-/usr/local/google_appengine/appcfg.py $1
+/usr/local/google_appengine/appcfg.py $1 $2
