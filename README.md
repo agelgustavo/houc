a50811083dbc4919f684e8f377fc34432b09850b
Gustavo Silverio <agelgustavo@gmail.com>
2015-06-26 09:29:44 -0300

    correcao de script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # houc
+
+To install virtualenv: pip install virtualenv
+To install virtualenvwrapper: pip install virtualenvwrapper
+
+export in your path: 
--- env/dev_server.sh
+++ env/dev_server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -d "../lib" ]; then
+if [ ! -d "lib" ]; then
     ln -s ~/.virtualenvs/houc/lib/python2.7/site-packages/ lib
 fi
 
